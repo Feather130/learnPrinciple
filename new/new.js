be37202364eed2f72a667a63b4dbefd059e66cfc
feather130 <1301788346@qq.com>
2021-04-11 11:48:09 +0800

    __proto__ 拼写错误

--- new/new.js
+++ new/new.js
@@ -1,7 +1,7 @@
 function objectFactory() {
     let obj = {}
     let Constructor = [].shift.call(arguments)
-    obj._proto_ = Constructor.prototype
+    obj.__proto__ = Constructor.prototype
     let ret = Constructor.apply(obj, arguments);
     return typeof ret === 'object' ? ret : obj;
 }
